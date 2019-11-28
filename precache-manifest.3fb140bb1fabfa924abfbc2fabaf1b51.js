95fd8639602b1308a56a405f8f274ec92c2381b1
Vincent Dang <vncntdang@gmail.com>
2019-11-28 10:51:29 -0500

    Updates

---
+++ precache-manifest.3fb140bb1fabfa924abfbc2fabaf1b51.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b31e12522e803271ba05802ac51a88d",
+    "url": "/git-pages-react-app/index.html"
+  },
+  {
+    "revision": "c4edc83cdce5d3d7c161",
+    "url": "/git-pages-react-app/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "566977cd107981fa4003",
+    "url": "/git-pages-react-app/static/js/2.f0e4f202.chunk.js"
+  },
+  {
+    "revision": "c4edc83cdce5d3d7c161",
+    "url": "/git-pages-react-app/static/js/main.da5d19f1.chunk.js"
+  },
+  {
+    "revision": "b0c6d7d4f574462798ed",
+    "url": "/git-pages-react-app/static/js/runtime-main.d2c988f6.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/git-pages-react-app/static/media/logo.25bf045c.svg"
+  }
+]);
